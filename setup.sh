572d52d82d4f9f0b890d64560b114a300cf665c9
ChanderG <chandergovind@gmail.com>
2016-02-23 16:16:39 +0530

    remove setup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-### Simple setup of all dotfiles
-ln -s `pwd`/.Xresources ~/.Xresources
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.bash_aliases ~/.bash_aliases
-ln -s `pwd`/.muttrc ~/.muttrc
